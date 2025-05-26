c3a3e5dd5bbf9ee36247a96fdaec0d6251a85eb2
projekty.git@gmail.com <56201394+ajczi14@users.noreply.github.com>
2025-05-26 18:08:11 +0200

    Ensure single active device per user

--- tracking_project/tracking_app/models.py
+++ tracking_project/tracking_app/models.py
@@ -8,9 +8,14 @@ class User(models.Model):
 
 
 class Device(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)  # id urządzenia jako string (można UUID)
+    id = models.CharField(max_length=100, primary_key=True)
     assigned_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='devices')
     active = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.active and self.assigned_user:
+            Device.objects.filter(assigned_user=self.assigned_user, active=True).exclude(id=self.id).update(active=False, assigned_user=None)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Device {self.id} (Active: {self.active})"
